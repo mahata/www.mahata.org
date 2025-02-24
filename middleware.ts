92a4118d901222828e6aa0c46dd858aaa7964b2b
Yasunori MAHATA <mahata777@gmail.com>
2025-02-24 23:59:43 +0900

    🔧 make edge runtime explicit

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,10 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {}
+export const config = {
+  runtime: "edge",
+};
+
+export function middleware(request: NextRequest) {
+  return NextResponse.next();
+}
