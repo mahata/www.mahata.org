f0cdcec830544f42147eba39bd18450dafca4127
Yasunori MAHATA <mahata777@gmail.com>
2025-02-25 00:02:02 +0900

    💚 edge to experimental-edge

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export const config = {
-  runtime: "edge",
+  runtime: "experimental-edge",
 };
 
 export function middleware(request: NextRequest) {
